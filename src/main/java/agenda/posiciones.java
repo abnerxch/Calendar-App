09a2490668c153766cb99fe62b4a204e0445f87a
Abner <axocop@ufm.edu>
2019-04-15 23:25:40 -0600

    get the Y-axis

--- src/main/java/agenda/posiciones.java
+++ src/main/java/agenda/posiciones.java
@@ -55,4 +55,58 @@ public class posiciones {
         return foobar;
     }
 
+    protected int getY(int foobar){
+        int foo=0;
+        switch(foobar){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                foo=1;
+                break;
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                foo=2;
+                break;
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+                foo=3;
+                break;
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+                foo=4;
+                break;
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+                foo=5;
+                break;
+            case 31:
+                foo=6;
+                break;
+
+
+        }
+
+        return foo;
+    }
+
+
 }

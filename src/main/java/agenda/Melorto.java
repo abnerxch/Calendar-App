ddc5fa98176a29bd5a88db8f91ef1d8ce6760158
Abner <axocop@ufm.edu>
2019-04-15 23:04:27 -0600

    Setting months (names)

--- src/main/java/agenda/Melorto.java
+++ src/main/java/agenda/Melorto.java
@@ -29,4 +29,37 @@ public class Melorto {
 
     }
 
+
+    private void setNombre(int foo){
+        switch(foo){
+            case 1:this.nombre="enero";
+                break;
+            case 2: this.nombre="febrero";
+                break;
+            case 3: this.nombre="marzo";
+                break;
+            case 4: this.nombre="abril";
+                break;
+            case 5: this.nombre="mayo";
+                break;
+            case 6: this.nombre="junio";
+                break;
+            case 7: this.nombre="julio";
+                break;
+            case 8: this.nombre="agosto";
+                break;
+            case 9: this.nombre="septiembre";
+                break;
+            case 10: this.nombre="octubre";
+                break;
+            case 11: this.nombre="septiembre";
+                break;
+            case 12: this.nombre="diciembre";
+                break;
+
+
+        }
+    }
+
+
 }

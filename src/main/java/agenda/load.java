5d623694d7b086cd4311325a1c7b28139fda3a72
KateyMG <46852046+KateyMG@users.noreply.github.com>
2019-04-22 18:55:51 -0600

    clase load lista

--- src/main/java/agenda/load.java
+++ src/main/java/agenda/load.java
@@ -1,7 +1,11 @@
 package agenda;
 
-import javax.swing.text.Document;
-import javax.swing.text.Element;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,12 +13,13 @@ public class load {
 
     public load(String path)
     {
-        SAXBUILDER builder = new SAXBUILDER(false);
-        try{
-            Document doc=builder.build(path);
-            Element raiz=doc.getRootElement();
+        SAXBuilder builder =new SAXBuilder(false);
+        //File xmlFile = new File("C:\\Users\\yohan\\IdeaProjects\\Calendar-App2\\src\\main\\java\\agenda");
 
-            List contactos=raiz.getChildren("contacto");
+        try{
+            Document document=(Document) builder.build(path);
+            Element rootNode=document.getRootElement();
+            List contactos=rootNode.getChildren("contacto");
             Iterator i = contactos.iterator();
 
             while(i.hasNext()){
@@ -23,7 +28,28 @@ public class load {
                 System.out.println(nombre.getText());
                 Element apellido=e.getChild("apellido");
                 System.out.println(apellido.getText());
+                Element pse=e.getChild("seudonimo");
+                System.out.println(pse.getText());
+                Element clas=e.getChild("clasificacion");
+                System.out.println(clas.getText());
+                Element tel=e.getChild("telefono");
+                System.out.println(tel.getText());
+                Element cel=e.getChild("celular");
+                System.out.println(cel.getText());
+                Element dir=e.getChild("direccion");
+                System.out.println(dir.getText());
+                Element email=e.getChild("email");
+                System.out.println(email.getText());
             }
+        } catch (JDOMException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+    public static void main(String args[]){
+
+        load cc=new load("contactos.xml");
+
     }
 }
